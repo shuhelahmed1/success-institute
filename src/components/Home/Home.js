9c82cf99cf2735e280e9908806de60184e43a52a
shuhelahm <shuhel037@gmail.com>
2021-10-04 20:20:05 +0600

    discount section added

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { useEffect } from 'react';
 import { Row } from 'react-bootstrap';
 import HomeComponents from '../HomeComponents/HomeComponents';
+import './Home.css';
 
 const Home = () => {
     const [homeComponents, setHomeComponents] = useState([]);
@@ -11,16 +12,38 @@ const Home = () => {
           .then(data => setHomeComponents(data))
       }, [])
     return (
-            <Row xs={1} md={2} className="g-4 w-75 m-auto">
-            {
-                homeComponents.map(homeComponent => <HomeComponents
-                key={homeComponent.key}
-                homeComponentName={homeComponent.name}
-                homeComponentPrice={homeComponent.price}
-                homeComponentImg={homeComponent.img}
-                ></HomeComponents>)
-            }
-</Row>
+    <div>
+        <div className='discount-section d-grid'>
+        <div className='discount-component d-flex'>
+            <div>
+            <img src="https://image.shutterstock.com/image-vector/grey-discount-20-sale-background-600w-1075991594.jpg" alt="" />
+            </div>
+            <div>
+                <h2>1. IELTS</h2>
+                <h2>2. GRE</h2>
+            </div>
+        </div>
+        <div className='discount-component d-flex'>
+            <div>
+            <img src="https://media.istockphoto.com/vectors/discount-sticker-sale-red-tag-isolated-vector-illustration-discount-vector-id831172744?s=612x612" alt="" />
+            </div>
+            <div>
+                <h2>1. Spoken English</h2>
+                <h2>2. Web Developement</h2>
+            </div>
+        </div>
+        </div>
+        <Row xs={1} md={2} className="g-4 w-75 m-auto">
+                    {
+                        homeComponents.map(homeComponent => <HomeComponents
+                        key={homeComponent.key}
+                        homeComponentName={homeComponent.name}
+                        homeComponentPrice={homeComponent.price}
+                        homeComponentImg={homeComponent.img}
+                        ></HomeComponents>)
+                    }
+        </Row>
+    </div>
     );
 };
 
--- src/components/HomeComponents/HomeComponents.js
+++ src/components/HomeComponents/HomeComponents.js
@@ -5,16 +5,16 @@ import './HomeComponents.css';
 const HomeComponents = (props) => {
     return (
         <Col>
-      <Card className='home-card'>
-        <Card.Img variant="top" src={props.homeComponentImg} />
-        <Card.Body>
-          <Card.Title>Course: {props.homeComponentName}</Card.Title>
-          <Card.Text>
-          Course fee: ${props.homeComponentPrice}
-          </Card.Text>
-        </Card.Body>
-      </Card>
-    </Col>  
+              <Card className='home-card'>
+                <Card.Img variant="top" src={props.homeComponentImg} />
+                <Card.Body>
+                  <Card.Title>Course: {props.homeComponentName}</Card.Title>
+                  <Card.Text>
+                  Course fee: ${props.homeComponentPrice}
+                  </Card.Text>
+                </Card.Body>
+              </Card>
+            </Col>      
     );
 };
 
