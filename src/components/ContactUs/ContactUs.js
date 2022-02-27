660c768610d601b55bc9e2bb0e5e43e1ad8e6fb7
shuhelahm <shuhel037@gmail.com>
2022-02-27 19:31:03 +0600

    updated

--- src/components/ContactUs/ContactUs.js
+++ src/components/ContactUs/ContactUs.js
@@ -13,8 +13,8 @@ const ContactUs = () => {
                     <img src="https://media.istockphoto.com/photos/smiling-indian-man-looking-at-camera-picture-id1270067126?b=1&k=20&m=1270067126&s=170667a&w=0&h=rq1zL5aHt9q4DtwW8jp2BrdTSOGKt7dGI8e5jham9R8=" alt="" />
                 </div>
                 <div>
-                    <h2>Name: Bilal Hasan</h2>
-                    <h3>Email: bilalpro@gmail.com</h3>
+                    <h2>Name: Bilal Ahmed</h2>
+                    <h3>Email: bilalahmed@gmail.com</h3>
                     <h3>Phone: 01789338458</h3>
                     <h3>Address: Zindabazar, Sylhet</h3>
                 </div>
